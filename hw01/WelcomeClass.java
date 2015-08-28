41f4c7297a3558ac5eef1ef4111c415e5ca4a759
stephengoelz1 <sag519@lehigh.edu>
2015-08-28 16:16:40 +0000

    created WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////
+//Stephen Goelz
+//Hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+
+// define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args)  {
+
+// print the statement with a box around it
+    System.out.println("    ----------------     ");
+    System.out.println("    |    WELCOME    |    ");
+    System.out.println("    -----------------    ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" /\\/\\/\\/\\/\\/\\ ");
+    System.out.println("|-S--A--G--5--1--9-|");
+    System.out.println(" \\/\\/\\/\\/\\/\\/ ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This summer I went to the");
+    System.out.println("Bahamas and spent my days");
+    System.out.println("cliff jumping and gambling");
+    
+    //test/testtest
+    }
+}
