4b656967752b89f6b8edb6d3a67de70054237520
stephengoelz1 <sag519@lehigh.edu>
2015-09-14 14:26:49 +0000

    updated Timer.java and Block.java

--- hw03/Block.java
+++ hw03/Block.java
@@ -5,22 +5,27 @@ import java.util.Scanner;
 public class Block {
     
     public static void main(String [] args){
-    
+    //create a scanenr so the user can input values
     Scanner DIMENSIONS   = new Scanner(System.in);
     
+    //user enters first value
     System.out.print("Height of block:");
         float h = DIMENSIONS.nextFloat ();
-        
+    //user enteres second value    
     System.out.print("Width of block:");
         float w = DIMENSIONS.nextFloat ();
-        
+    //third value    
     System.out.print("Length of block:");
         float l = DIMENSIONS.nextFloat();
         
+    //program calculates volume
     float volume = l *  w * h;
     
+    //program calculates surface area
     float SA = (2*(l * w)) + (2*(w * h)) + (2*(h * l));
     
+    
+    //values are printed for the user to read
     System.out.println("Volume:" + volume);
     System.out.println("Surface Area:" + SA);
     }
--- hw03/Timer.java
+++ hw03/Timer.java
@@ -7,16 +7,24 @@ public class Timer {
 
     public static void main(String [] args){
         
+        //create scanner for user input
         Scanner TIME = new Scanner(System.in);
         
+        //enters current time
         System.out.print("Current time:"); 
             int timeCurrent = TIME.nextInt ();
             
+        //user enters dinner time
         System.out.print("Dinner time:");
             int timeDinner = TIME.nextInt ();
             
+        //computes hours until dinner
         int timeUntilHours = (timeDinner - timeCurrent) / 100;
+        
+        //finds minute remainder to be displayed
         int timeUntilMinutes = (timeDinner - timeCurrent) % 100;
+        
+        //information is displayed
         System.out.println ("You have " + timeUntilHours + " hours and " + timeUntilMinutes + " minutes until dinner.");
         
         
